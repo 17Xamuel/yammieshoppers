f99f24aff1ea1d85f1fb32d88aff43495e24233b
unknown <samweko@gmail.com>
2020-10-06 10:20:07 +0300

    - index.js changed

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const multer = require("multer");
+const mysql = require("mysql");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: "./uploads/images/product_images/",
+});
 
 const PORT = process.env.PORT || 800;
 const app = express();
 
 app.use(express.static("public"));
+
+app.post("/upload", (req, res) => {});
 
 app.listen(PORT, () => {
   console.log(`Server started on ${PORT}...`);
--- public/seller/scripts/addproduct.js
+++ public/seller/scripts/addproduct.js
@@ -1,44 +1,44 @@
-let productForm = document.getElementById("productForm");
-productForm.addEventListener("submit", (e) => {
-  e.preventDefault();
+// let productForm = document.getElementById("productForm");
+// productForm.addEventListener("submit", (e) => {
+//   e.preventDefault();
 
-  let Product = document.getElementById("productname").value;
-  let Price = document.getElementById("price").value;
-  let Description = document.getElementById("description").value;
-  let Brand = document.getElementById("productBrand").value;
-  let Category = document.getElementById("category").value;
-  let Subcategory = document.getElementById("subcategory").value;
-  let Discount = document.getElementById("discount").value;
-  let ImageList = document.querySelector("[name=images]").files;
+//   let Product = document.getElementById("productname").value;
+//   let Price = document.getElementById("price").value;
+//   let Description = document.getElementById("description").value;
+//   let Brand = document.getElementById("productBrand").value;
+//   let Category = document.getElementById("category").value;
+//   let Subcategory = document.getElementById("subcategory").value;
+//   let Discount = document.getElementById("discount").value;
+//   let ImageList = document.querySelector("[name=images]").files;
 
-  const formData = new FormData();
-  formData.append("Product", Product);
-  formData.append("Price", Price);
-  formData.append("Description", Description);
-  formData.append("Brand", Brand);
-  formData.append("Category", Category);
-  formData.append("Subcategory", Subcategory);
-  formData.append("Discount", Discount);
+//   const formData = new FormData();
+//   formData.append("Product", Product);
+//   formData.append("Price", Price);
+//   formData.append("Description", Description);
+//   formData.append("Brand", Brand);
+//   formData.append("Category", Category);
+//   formData.append("Subcategory", Subcategory);
+//   formData.append("Discount", Discount);
 
-  for (var i = 0; i < ImageList.length; i++) {
-    formData.append(`Image${i}`, ImageList[i]);
-  }
+//   for (var i = 0; i < ImageList.length; i++) {
+//     formData.append(`Image${i}`, ImageList[i]);
+//   }
 
-  let productData = {};
-  for (var key of formData.keys()) {
-    productData[key] = formData.get(key);
-  }
+//   let productData = {};
+//   for (var key of formData.keys()) {
+//     productData[key] = formData.get(key);
+//   }
 
-  const xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = () => {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      document.getElementById("messages").innerHTML = xhr.responseText;
-    }
-  };
+//   const xhr = new XMLHttpRequest();
+//   xhr.onreadystatechange = () => {
+//     if (xhr.readyState == 4 && xhr.status == 200) {
+//       document.getElementById("messages").innerHTML = xhr.responseText;
+//     }
+//   };
 
-  let productInfo = JSON.stringify(productData);
+//   let productInfo = JSON.stringify(productData);
 
-  xhr.open("POST", "http://localhost:3000/api/sellers/addProduct", true);
-  xhr.setRequestHeader("Content-Type", "application/json");
-  xhr.send(productInfo);
-});
+//   xhr.open("POST", "http://localhost:3000/api/sellers/addProduct", true);
+//   xhr.setRequestHeader("Content-Type", "application/json");
+//   xhr.send(productInfo);
+// });
