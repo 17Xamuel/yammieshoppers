0a6726e95edee5b1a914562dc6249caa52a38fd8
MulindwaDenis13 <55537331+MulindwaDenis13@users.noreply.github.com>
2020-10-07 03:13:09 -0800

    Seller modified

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function getUpload(id) {
   return upload;
 }
 
-const PORT = process.env.PORT || 800;
+const PORT = process.env.PORT || 8000;
 const app = express();
 
 app.use(express.static("public"));
@@ -42,37 +42,36 @@ app.post("/addProduct", async (req, res) => {
   upload(req, res, (err) => {
     if (err) throw err;
     console.log(req.files);
-  });
-  console.log(productId);
-  const {
-    product,
-    price,
-    description,
-    brand,
-    category,
-    subcategory,
-    discount,
-  } = req.body;
-  conn.query(
-    "INSERT INTO pending_products SET ?",
-    {
-      id: productId,
-      product: product,
-      price: price,
-      description: description,
-      brand: brand,
-      category: category,
-      subcategory: subcategory,
-      discount: discount,
-    },
-    (err, results) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.send("Product Added");
+    const {
+      product,
+      price,
+      description,
+      brand,
+      category,
+      subcategory,
+      discount,
+    } = req.body;
+    conn.query(
+      "INSERT INTO pending_products SET ?",
+      {
+        id: productId,
+        product: product,
+        price: price,
+        description: description,
+        brand: brand,
+        category: category,
+        subcategory: subcategory,
+        discount: discount,
+      },
+      (err, results) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send("Product Added");
+        }
       }
-    }
-  );
+    );
+  });
 });
 
 app.listen(PORT, () => {
