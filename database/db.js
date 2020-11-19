456e6c34f84973dbc4fee04a3211be247dacd87e
Xamuel Wekobosya <samweko@gmail.com>
2020-11-19 14:07:35 +0300

    db

--- database/db.js
+++ database/db.js
@@ -3,9 +3,9 @@ const mysql = require("mysql");
 
 const conn = mysql.createConnection({
   host: "us-cdbr-east-02.cleardb.com",
-  user: "b838eb930c84a9",
-  password: "3af0fe60",
-  database: "heroku_f4061a257bbaada",
+  user: "b2ed4a370a3aae",
+  password: "0db01b7e",
+  database: "heroku_ddb34418618ebc1",
 });
 
 conn.connect((err) => {
