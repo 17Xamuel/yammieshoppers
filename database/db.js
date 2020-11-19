272cf609b000acb6ed57b291138c2df1788a9257
Xamuel Wekobosya <samweko@gmail.com>
2020-11-19 22:17:19 +0300

    DBB

--- database/db.js
+++ database/db.js
@@ -2,10 +2,10 @@ const express = require("express");
 const mysql = require("mysql");
 
 const conn = mysql.createConnection({
-  host: "us-cdbr-east-02.cleardb.com",
-  user: "b2ed4a370a3aae",
-  password: "0db01b7e",
-  database: "heroku_ddb34418618ebc1",
+  host: "yammie-do-user-8336351-0.b.db.ondigitalocean.com",
+  user: "doadmin",
+  password: "nw61grac0k3gmh9v",
+  database: "defaultdb",
 });
 
 conn.connect((err) => {
