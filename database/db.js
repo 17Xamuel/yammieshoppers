e28b9789efd0f807dcbc6e07ac059e7ee576419c
Xamuel Wekobosya <samweko@gmail.com>
2020-11-21 17:53:32 +0300

    deploy

--- database/db.js
+++ database/db.js
@@ -1,12 +1,18 @@
-const express = require("express");
+const path = require("path");
+const fs = require("fs");
 const mysql = require("mysql");
 
-const conn = mysql.createConnection({
-  host: "yammie-do-user-8336351-0.b.db.ondigitalocean.com",
-  user: "doadmin",
-  password: "nw61grac0k3gmh9v",
-  database: "defaultdb",
-});
+const MYSQL_CONFIG = {
+  host: "yammie-db-secure-do-user-8336351-0.b.db.ondigitalocean.com",
+  user: "yammie",
+  password: "osbzf7q7nqf7qdal",
+  database: "yammie_db",
+  port: 25060,
+  ssl: {
+    ca: fs.readFileSync(path.join(__dirname, "ca-certificate.crt")),
+  },
+};
+const conn = mysql.createConnection(MYSQL_CONFIG);
 
 conn.connect((err) => {
   if (err) throw err;
