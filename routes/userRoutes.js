667a743b63c36a9d1fa68c7a26881c2ed20e84c9
MulindwaDenis13 <mulindwadenis13@gmail.com>
2021-01-01 22:30:31 +0300

    Quantity change

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -283,38 +283,63 @@ router.post("/customer/order", async (req, res) => {
             let itemdetails = Object.values(items);
             itemdetails.forEach((item) => {
               conn.query(
-                `SELECT * FROM seller_orders WHERE Product_id= ${item.cartItemadded}`,
-                (errr, result_0) => {
-                  if (errr) throw errr;
-                  if (result_0.length == 0) {
+                `SELECT * FROM products WHERE id = '${item.cartItemAdded}'`,
+                (qerr, qresult) => {
+                  if (qerr) throw qerr;
+                  if (qresult.length > 0) {
                     conn.query(
-                      `INSERT INTO seller_orders SET ? `,
-                      {
-                        id: uuid.v4(),
-                        product_id: item.cartItemAdded,
-                        order_price: item.price,
-                        order_product: item.name,
-                        order_discount: item.discount,
-                        order_qty: item.inCartNumber,
-                        order_amount:
-                          (item.price - (item.discount / 100) * item.price) *
-                          item.inCartNumber
-                      },
-                      (error, result_3) => {
-                        if (error) throw error;
-                      }
-                    );
-                  } else if (result_0.length > 0) {
-                    conn.query(
-                      `SELECT order_qty FROM seller_orders WHERE product_id=${item.cartItemAdded}`,
-                      (err_0, res_0) => {
-                        if (err_0) throw err_0;
-                        let finalQuantity =
-                          res_0[0].order_qty + item.inCartNumber;
+                      `SELECT quantity FROM products WHERE id = '${item.cartItemAdded}'`,
+                      (errq, resultq) => {
+                        if (errq) throw errq;
+                        let quantityChange =
+                          resultq[0].quantity - item.inCartNumber;
                         conn.query(
-                          `UPDATE seller_orders SET order_order_qty=${finalQuantity} WHERE product_id=${item.cartItemAdded}`,
-                          (err_1, res_1) => {
-                            if (err_1) throw err_1;
+                          `UPDATE products SET quantity =${quantityChange} WHERE id = '${item.cartItemAdded}'`,
+                          (q_err, q_result) => {
+                            if (q_err) throw q_err;
+                            conn.query(
+                              `SELECT * FROM seller_orders WHERE Product_id= ${item.cartItemadded}`,
+                              (errr, result_0) => {
+                                if (errr) throw errr;
+                                if (result_0.length == 0) {
+                                  conn.query(
+                                    `INSERT INTO seller_orders SET ? `,
+                                    {
+                                      id: uuid.v4(),
+                                      product_id: item.cartItemAdded,
+                                      order_price: item.price,
+                                      order_product: item.name,
+                                      order_discount: item.discount,
+                                      order_qty: item.inCartNumber,
+                                      order_amount:
+                                        (item.price -
+                                          (item.discount / 100) * item.price) *
+                                        item.inCartNumber
+                                    },
+                                    (error, result_3) => {
+                                      if (error) throw error;
+                                    }
+                                  );
+                                } else if (result_0.length > 0) {
+                                  conn.query(
+                                    `SELECT order_qty FROM seller_orders 
+                                    WHERE product_id=${item.cartItemAdded}`,
+                                    (err_0, res_0) => {
+                                      if (err_0) throw err_0;
+                                      let finalQuantity =
+                                        res_0[0].order_qty + item.inCartNumber;
+                                      conn.query(
+                                        `UPDATE seller_orders SET order_order_qty=${finalQuantity} WHERE 
+                                        product_id=${item.cartItemAdded}`,
+                                        (err_1, res_1) => {
+                                          if (err_1) throw err_1;
+                                        }
+                                      );
+                                    }
+                                  );
+                                }
+                              }
+                            );
                           }
                         );
                       }
