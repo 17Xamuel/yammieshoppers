4eca4f987f46c326e9f80ff08c51af6a9a5b77cb
XamuelUG <samweko@gmail.com>
2021-01-08 12:58:23 +0300

    err

--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -6,20 +6,22 @@ const router = express.Router();
 const charge = require("./charges");
 
 router.post("/updateCart/:id", (req, res) => {
-  conn.query(
-    "UPDATE customers SET ? where c_id = ?",
-    [
-      {
-        c_cart: req.body[0],
-        c_cart_number: req.body[1],
-      },
-      req.params.id,
-    ],
-    (err, result) => {
-      if (err) throw err;
-      if (result.affectedRows > 0) res.status(200).send("cart amount upated");
-    }
-  );
+  console.log(typeof req.body[0]);
+  console.log(typeof req.body);
+  //   conn.query(
+  //     "UPDATE customers SET ? where c_id = ?",
+  //     [
+  //       {
+  //         c_cart: req.body[0],
+  //         c_cart_number: req.body[1],
+  //       },
+  //       req.params.id,
+  //     ],
+  //     (err, result) => {
+  //       if (err) throw err;
+  //       if (result.affectedRows > 0) res.status(200).send("cart amount upated");
+  //     }
+  //   );
 });
 
 module.exports = router;
