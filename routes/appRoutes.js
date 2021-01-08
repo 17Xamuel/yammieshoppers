5fec715e23b13ba21931bd58b838b7ca596f6486
Samuel Wekobosya <61344732+XamuelUG@users.noreply.github.com>
2021-01-08 13:55:15 +0300

    Update appRoutes.js

--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -6,22 +6,20 @@ const router = express.Router();
 const charge = require("./charges");
 
 router.post("/updateCart/:id", (req, res) => {
-  console.log(typeof req.body[0]);
-  console.log(req.body);
-  //   conn.query(
-  //     "UPDATE customers SET ? where c_id = ?",
-  //     [
-  //       {
-  //         c_cart: req.body[0],
-  //         c_cart_number: req.body[1],
-  //       },
-  //       req.params.id,
-  //     ],
-  //     (err, result) => {
-  //       if (err) throw err;
-  //       if (result.affectedRows > 0) res.status(200).send("cart amount upated");
-  //     }
-  //   );
+     conn.query(
+       "UPDATE customers SET ? where c_id = ?",
+       [
+        {
+          c_cart: req.body[0],
+          c_cart_number: req.body[1],
+        },
+        req.params.id,
+      ],
+      (err, result) => {
+         if (err) throw err;
+        if (result.affectedRows > 0) res.status(200).send("cart amount upated");
+       }
+    );
 });
 
 module.exports = router;
