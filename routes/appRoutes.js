50f8bb17d60cf7d8a6ba6a1b7ae67a27e14b06b1
XamuelUG <samweko@gmail.com>
2021-01-20 22:37:00 +0300

    error

--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -22,63 +22,62 @@ router.post("/updateCart/:id", (req, res) => {
   );
 });
 
-router.post("/checkout/cart/:id", (req, res) => {
-  console.log(req.body);
-  // conn.query(
-  //   `SELECT c_cart_amount,c_cart,c_cart_number,zone
-  //     FROM customers JOIN customer_address
-  //     ON customer_address.c_id = customers.c_id WHERE customers.c_id = ?`,
-  //   req.params.id,
-  //   (err, result) => {
-  //     if (err) {
-  //       throw err;
-  //     } else {
-  //       let cart = JSON.parse(result[0].c_cart);
-  //       let total_charge = 0;
-  //       let total_price = 0;
-  //       let cart_arr = Object.values(cart);
-  //       for (let i = 0; i < cart_arr.length; i++) {
-  //         let key = cart_arr[i];
-  //         conn.query(
-  //           `SELECT * FROM products WHERE id = ?`,
-  //           key.cartItemAdded,
-  //           (error, result_0) => {
-  //             if (error) {
-  //               throw error;
-  //             } else {
-  //               let _charge = 0;
-  //               let product =
-  //                 JSON.parse(result_0[0].specifications) == null
-  //                   ? { Size: "small", Fragile: "No", weight: "light" }
-  //                   : JSON.parse(result_0[0].specifications);
+router.get("/checkout/cart/:id", (req, res) => {
+  conn.query(
+    `SELECT c_cart_amount,c_cart,c_cart_number,zone
+      FROM customers JOIN customer_address
+      ON customer_address.c_id = customers.c_id WHERE customers.c_id = ?`,
+    req.params.id,
+    (err, result) => {
+      if (err) {
+        throw err;
+      } else {
+        let cart = JSON.parse(result[0].c_cart);
+        let total_charge = 0;
+        let total_price = 0;
+        let cart_arr = Object.values(cart);
+        for (let i = 0; i < cart_arr.length; i++) {
+          let key = cart_arr[i];
+          conn.query(
+            `SELECT * FROM products WHERE id = ?`,
+            key.cartItemAdded,
+            (error, result_0) => {
+              if (error) {
+                throw error;
+              } else {
+                let _charge = 0;
+                let product =
+                  JSON.parse(result_0[0].specifications) == null
+                    ? { Size: "small", Fragile: "No", weight: "light" }
+                    : JSON.parse(result_0[0].specifications);
 
-  //               let charge_obj = {
-  //                 price: result_0[0].discount
-  //                   ? (result_0[0].price * (100 - result_0[0].discount)) / 100
-  //                   : result_0[0].price,
-  //                 qty: key.inCartNumber < 4 ? "few" : "many",
-  //                 urgent: req.body.urgent || true,
-  //                 size: product.Size || "small",
-  //                 fragile: (product.Fragile == "Yes" ? true : false) || false,
-  //                 location: "Lira",
-  //                 weight: product.Weight || "Light",
-  //                 user: req.body._add == true ? result[0].zone : req.body._add,
-  //               };
+                let charge_obj = {
+                  price: result_0[0].discount
+                    ? (result_0[0].price * (100 - result_0[0].discount)) / 100
+                    : result_0[0].price,
+                  qty: key.inCartNumber < 4 ? "few" : "many",
+                  urgent: true,
+                  size: product.Size || "small",
+                  fragile: (product.Fragile == "Yes" ? true : false) || false,
+                  location: "Lira",
+                  weight: product.Weight || "Light",
+                  user: result[0].zone,
+                };
 
-  //               let fee = new charge(charge_obj).total;
-  //               let price = new charge(charge_obj).price;
-  //               total_charge += fee;
-  //               total_price += price * key.inCartNumber;
-  //               if (cart_arr.length == i + 1) {
-  //                 res.send({ shipping: total_charge, total_price });
-  //               }
-  //             }
-  //           }
-  //         );
-  //       }
-  //     }
-  //   }
-  // );
+                let fee = new charge(charge_obj).total;
+                let price = new charge(charge_obj).price;
+                total_charge += fee;
+                total_price += price * key.inCartNumber;
+                if (cart_arr.length == i + 1) {
+                  res.send({ shipping: total_charge, total_price });
+                }
+              }
+            }
+          );
+        }
+      }
+    }
+  );
 });
 
 module.exports = router;
