565beefb585c7c37166a2a7ec8db7b4c0ea4391e
XamuelUG <samweko@gmail.com>
2021-02-08 22:28:07 +0300

    error

--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -179,6 +179,7 @@ router.get("/checkout/cart/:id", (req, res) => {
         throw err;
       } else {
         let cart = JSON.parse(result[0].c_cart);
+        console.log(cart);
         let total_charge = 0;
         let total_price = 0;
         let cart_arr = Object.values(cart);
@@ -191,6 +192,7 @@ router.get("/checkout/cart/:id", (req, res) => {
               if (error) {
                 throw error;
               } else {
+                console.log(result_0[0]);
                 let _charge = 0;
                 let product =
                   JSON.parse(result_0[0].specifications) == null
